2a40d5eed664c4c3209dfc52a947441ab7395fc5
uuccu <sungu0804@gmail.com>
2024-04-03 15:42:14 +0900

    Update EmailVerificationService.kt

--- src/main/kotlin/fittibackendapp/domain/auth/service/EmailVerificationService.kt
+++ src/main/kotlin/fittibackendapp/domain/auth/service/EmailVerificationService.kt
@@ -1,6 +1,7 @@
 package fittibackendapp.domain.auth.service
 
 import fittibackendapp.configuration.EmailConfiguration
+import org.apache.commons.lang3.RandomStringUtils
 import org.reflections.Reflections.log
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.mail.SimpleMailMessage
@@ -15,10 +16,11 @@ class EmailVerificationService(
     fun sendMessage(email: String) {
         val message = SimpleMailMessage()
         message.setTo(email)
-        message.text = "Hello, this is a test email"
-        message.subject = "Test email"
 
-
+        var code = RandomStringUtils.randomNumeric(4)
+        message.text = "$EMAIL_CONTENT\n인증번호: $code"
+        message.subject = EMAIL_TITLE
+        
         try {
             emailSender.send(message)
         } catch (
@@ -26,6 +28,11 @@ class EmailVerificationService(
         ) {
             log.debug("Failed to send email to $email")
         }
+    }
+
+    companion object {
+        const val EMAIL_TITLE = "Fitti-이메일 인증 요청"
+        const val EMAIL_CONTENT = "Fitti에 가입해주셔서 감사합니다. 아래의 인증번호를 입력하여 이메일 인증을 완료해주세요."
     }
 }
 
