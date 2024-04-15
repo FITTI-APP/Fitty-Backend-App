72176d14669f732c17c5c4c9091634ee8eda4b26
uuccu <sungu0804@gmail.com>
2024-04-15 23:12:59 +0900

    Update EmailVerificationService.kt

--- src/main/kotlin/fittibackendapp/domain/auth/service/EmailVerificationService.kt
+++ src/main/kotlin/fittibackendapp/domain/auth/service/EmailVerificationService.kt
@@ -8,6 +8,7 @@ import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
 
 @Service
 class EmailVerificationService(
